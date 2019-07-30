280636c3accaf52c9790a11a9835c331e34f07f3
aleksalukac <aleksa@lukac.rs>
2019-07-30 22:55:27 +0200

    changed to 64 base

--- binaryFileEncriptionWith4LetterAlphabet/binaryFileEncriptionWith4LetterAlphabet.cpp
+++ binaryFileEncriptionWith4LetterAlphabet/binaryFileEncriptionWith4LetterAlphabet.cpp
@@ -13,16 +13,58 @@ using namespace std;
 
 #pragma warning(disable:4996)
 
-int cnvrt(int buf)
+void reverseString(string &stringToReverse)
 {
-	switch (buf) 
+	int numCharacters = stringToReverse.length();
+	for (int i = 0; i < numCharacters / 2; i++)
 	{
+		std::swap(stringToReverse[i], stringToReverse[numCharacters - i - 1]);
+	}
+}
+
+string toBin(int value)
+{
+	string binaryNum = "000000";
+
+	int i = 0;
+	while (value > 0) {
+
+		binaryNum[i] = (value % 2) + '0';
+		value = value / 2;
+		i++;
+	}
+	reverseString(binaryNum);
+
+	return binaryNum;
+}
+
+int cnvrt(string buf)
+{
+	if (buf.size() < 6)
+	{
+		int bufSize = buf.size();
+		for (int i = 0; i < (6 - bufSize); i++)
+		{
+			buf += '0';
+		}
+	}
+	int k = 0;
+	int g = 32;
+	for (int i = 0; i < 6; i++)
+	{
+		k += g * (buf[i] - '0');
+		g /= 2;
+	}
+	return k;
+	/*switch (buf) 
+	{
+
 	case 0: return 0;
 	case 1: return 1;
 	case 16: return 2;
 	case 17: return 3;
 	case 204: return -1;
-	}
+	}*/
 }
 
 string getFileName(string fileName)
@@ -41,33 +83,75 @@ string getFileExtension(string fileName)
 
 bool encrypt(string fileName) // test : fileName = "test.bin"
 {
-	unsigned char buffer[1000];
-	FILE *ptr;
+	FILE *file = fopen(fileName.c_str(), "r");
+	char *code;
+	size_t n = 0;
+	int c;
 
-	ptr = fopen(fileName.c_str(), "rb");  // r for read, b for binary
+	if (file == NULL) return NULL; //could not open file
+	fseek(file, 0, SEEK_END);
+	long f_size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	code = (char*)malloc(f_size);
 
-	cout << "Enter the alphabet (4 chars): " << endl;
+	while ((c = fgetc(file)) != EOF) {
+		code[n++] = (char)c;
+	}
 
-	char alphabet[16];
+	code[n] = '\0';
+
+	cout << "Enter the alphabet (64 chars): " << endl;
+
+	string alphabet[64];
 	
-	for (int i = 0; 4 - i; i++)
+	for (int i = 0; 64 - i; i++)
 	{
 		cout << i + 1 << ". ";
 		cin >> alphabet[i];
-	}
 
-	fread(buffer, sizeof(buffer), 4, ptr);
+		// commended code is for faster testing
+		/*string c = ""; 
+		if (i < 10)
+			c += "0";
+		c += to_string(i);
+		alphabet[i] = c;*/
+	}
 	
 	FILE * encripted = fopen((getFileName(fileName) + "_encripted.txt").c_str(), "w");
 	
-	for (int i = 0; i < 100; i++)
+	string text = ""; //debugging
+
+	int i = 0;
+	while (i < n)
 	{
-		int cvr = cnvrt(buffer[i]);
-		cvr = cvr < 0 ? -1 : fprintf(encripted, "%c", alphabet[cnvrt(buffer[i])]);
+		
+		int j = 0;
+		string s;
+		if (i == 0)
+		{
+			s += code[0];
+			j++;
+		}
+
+		while (j++ < 6)
+		{
+			i++;
+			if (i >= n) break;
+			s += code[i];
+		}
+
+		//if (i >= n) break;
+
+		text += alphabet[cnvrt(s)];
 	}
 
+	fprintf(encripted, "%s", text.c_str());
+	//fputs(text, encripted);
+
+	fclose(encripted);
 	return true;
 }
+
 
 bool decrypt(string fileName) // test : fileName = "test_encripted.txt"
 {
@@ -88,53 +172,51 @@ bool decrypt(string fileName) // test : fileName = "test_encripted.txt"
 
 	code[n] = '\0';
 
-	cout << "Enter the alphabet (4 chars): " << endl;
+	cout << "Enter the alphabet (64 pairs of chars, example: 'ab'): " << endl;
 
-	unordered_map<char, int> alphabet;
+	unordered_map<string, int> alphabet;
 
 
-	for (int i = 0; 4 - i; i++)
+	for (int i = 0; 64 - i; i++)
 	{
 		cout << i + 1 << ". ";
 		char c;
 		cin >> c;
 		alphabet[c] = i;
+
+		// commended code is for faster testing
+		/*
+		string c = "";
+		if (i < 10)
+			c += "0";
+		c += to_string(i);
+		alphabet[c] = i;*/
 	}
 
 	FILE *decripted;
 
 	decripted = fopen((getFileName(fileName) + "_decripted.text").c_str(), "w");  // w for write, b for binary
 	
-	for (int i = 0; i < n; i++)
-	{
-		int k = alphabet[code[i]];
-		if (k != 204)
-		{
-			if (k / 2 == 0)
-			{
-				fprintf(decripted, "%i", 0);
-			}
-			else
-			{
-				fprintf(decripted, "%i", 1);
-			}
+	string text = "";
 
-			if (k % 2 == 0)
-			{
-				fprintf(decripted, "%i", 0);
-			}
-			else
-			{
-				fprintf(decripted, "%i", 1);
-			}
-		}		
+	for (int i = 0; i < n / 2; i++)
+	{
+		string s = "";
+		s += code[i * 2];
+		s += code[i * 2 + 1];
+		int k = alphabet[s];
+		text += toBin(k);
 	}
 
+	fprintf(decripted, "%s", text.c_str());
 	return true;
 }
 
 int main()
 {
+	//cnvrt("1101");
+	toBin(1);
+
 	cout << "Enter file name:";
 	string fileName;
 	cin >> fileName;
